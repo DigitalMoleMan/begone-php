fa1c795d7a3c7ac54398e6e2da98d172375e7700
DigitalMoleMan <piwi16@nti.umea>
2018-09-19 11:17:34 +0200

    ok this is ephp

--- login.php
+++ login.php
@@ -1,28 +1,28 @@
 <?php
 session_start();
-if(isset($_POST['submit'])){
+if(isset($_POST['submit']))
+{
     $loginCredentials = [
-        'username' => "admin",
-        'password' => "admin"
+        "username" => "admin",
+        "password" => password_hash("admin", PASSWORD_DEFAULT)
     ];
 
-    if(isset($_POST['username']) && isset($_POST['password'])) {
+    if(isset($_POST['username']) && isset($_POST['password']))
+    {
 
-        $username = filter_input(INPUT_POST, "username", FILTER_SANETIZE_STRING);
-        $password = md5($_POST['password']);
+        $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+        $password = $_POST['password'];
 
         if($username == $loginCredentials['username']
-        && $password == $loginCredentials['password'])
+        && password_verify($password, $loginCredentials['password']))
         {
             echo "<h1>nice</h1>";
         } 
-
         elseif ($username != $loginCredentials['username']
-        || ($password != $loginCredentials['password'])
+        || !$password_verify($password, $loginCredentials['password']))
         {
             echo "<h1>oof</h1>";
         }
-    
-    echo "<pre>" . var_dump($_POST) . "</pre>";
+    }
 }
 ?>
